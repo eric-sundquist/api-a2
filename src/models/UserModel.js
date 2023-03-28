a77f6babebd5158e791e9d16417789eb9f11fc33
Eric Sundqvist <es225kz@student.lnu.se>
2023-03-28 19:15:54 +0200

    Update userModel

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -6,26 +6,43 @@
  */
 
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 // Create a schema.
 const schema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: [true, 'Username is required.'],
       unique: true,
-      trim: true,
-      minlength: 3
+      // - A valid username should start with an alphabet so, [A-Za-z].
+      // - All other characters can be alphabets, numbers or an underscore so, [A-Za-z0-9_-].
+      // - Since length constraint is 3-256 and we had already fixed the first character, so we give {2, 255}.
+      // - We use ^ and $ to specify the beginning and end of matching.
+      match: [
+        /^[A-Za-z][A-Za-z0-9_-]{2,255}$/,
+        'Please provide a valid username.'
+      ]
     },
     firstName: {
       type: String,
-      required: true,
-      minlength: 2
+      required: [true, 'First name is required.'],
+      minLength: [1, 'The first name must be of minimum length 1 characters.'],
+      maxLength: [
+        256,
+        'The first name must be of maximum length 256 characters.'
+      ],
+      trim: true
     },
     lastName: {
       type: String,
-      required: true,
-      minlength: 2
+      required: [true, 'Last name is required.'],
+      minLength: [1, 'The last name must be of minimum length 1 characters.'],
+      maxLength: [
+        256,
+        'The last name must be of maximum length 256 characters.'
+      ],
+      trim: true
     },
     password: {
       type: String,
@@ -65,6 +82,24 @@ const convertOptions = {
 schema.set('timestamps', true)
 schema.set('toObject', convertOptions)
 schema.set('toJSON', convertOptions)
+
+/**
+ * Authenticates a user.
+ *
+ * @param {string} username - ...
+ * @param {string} password - ...
+ * @returns {Promise<User>} ...
+ */
+schema.statics.authenticate = async function (username, password) {
+  const user = await this.findOne({ username })
+  // If no user found or password is wrong, throw an error.
+  if (!user || !(await bcrypt.compare(password, user?.password))) {
+    throw new Error('Invalid credentials.')
+  }
+
+  // User found and password correct, return the user.
+  return user
+}
 
 // Create a model using the schema.
 export const TaskModel = mongoose.model('User', schema)
